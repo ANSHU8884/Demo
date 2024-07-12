cca2abd753f23869b290380df8eab8fd48f93018
Anshu Kasar <106432171+ANSHU8884@users.noreply.github.com>
2024-07-12 22:59:15 +0530

    Add files via upload

---
+++ progam/src/main/java/com/springboot/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.entity.Mobile;
+import com.springboot.service.MobileService;
+
+//Fill your code here
+public class HomeController {
+	
+	//Fill your code here
+
+	//Fill your code here
+	public String getAllMovies(Model model) {
+		//Fill your code here
+	}
+
+}
